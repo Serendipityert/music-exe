074970cb8d44a9d4621683c1290bd494cfbd2138
Serendipityert <2978772379@qq.com>
2024-07-04 11:38:14 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,150 +1,155 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const publicRoutes = [
-    {
-        path: '/',
-        name: 'Index',
-        meta: {},
-        component: () => import('@/layout/index.vue'),
-        children: [{
-            path: '/',
-            name: 'Recommend',
-            meta: { isLogin: true },
-            component: () => import('@/view/recommend/index.vue')
-        }, {
-            path: '/music-hall',
-            name: 'MusicHall',
-            meta: { isLogin: false },
-            component: () => import('@/view/music-hall/index.vue')
-        },
-        {
-            path: '/video',
-            name: 'Video',
-            meta: { isLogin: false },
-            component: () => import('@/view/video/index.vue')
-        },
-        {
-            path: '/radio-station',
-            name: 'RadioStation',
-            meta: { isLogin: false },
-            component: () => import('@/view/radio-station/index.vue')
-        },
-        {
-            path: '/i-like',
-            name: 'ILike',
-            meta: { isLogin: true },
-            component: () => import('@/view/i-like/index.vue')
-        },
-        {
-            path: '/local-download',
-            name: 'LocalDownload',
-            meta: { isLogin: false },
-            component: () => import('@/view/local-download/index.vue')
-        },
-        {
-            path: '/recently-played',
-            name: 'RecentlyPlayed',
-            meta: { isLogin: false },
-            component: () => import('@/view/recently-played/index.vue')
-        },
-        {
-            path: '/audition-list',
-            name: 'AuditionList',
-            meta: { isLogin: false },
-            component: () => import('@/view/audition-list/index.vue')
-        },
-        {
-            path: '/purchased-music',
-            name: 'PurchasedMusic',
-            meta: { isLogin: true },
-            component: () => import('@/view/purchased-music/index.vue')
-        },
-        {
-            path: '/play-mv',
-            name: 'PlayMv',
-            meta: { isLogin: false },
-            component: () => import('@/view/mv/index.vue')
-        },
-        {
-            path: '/music-comment',
-            name: 'MusicComment',
-            meta: { isLogin: false },
-            component: () => import('@/view/music-comment/index.vue')
-        },
-        {
-            path: '/theme',
-            name: 'Theme',
-            meta: { isLogin: false },
-            component: () => import('@/view/theme/index.vue')
-        },
-        {
-            path: '/user-main',
-            name: 'UserMain',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/index.vue')
-        },
-        {
-            path: '/songer-detail',
-            name: 'SongerDetail',
-            meta: { isLogin: false },
-            component: () => import('@/components/songer/index.vue')
-        },
-        {
-            path: '/songer-album-detail',
-            name: 'AlbumDetail',
-            meta: { isLogin: false },
-            component: () => import('@/components/songer/album/album-detail/index.vue')
-        },
-        {
-            path: '/user-level-detail',
-            name: 'UserLevelDetail',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/level/index.vue')
-        },
-        {
-            path: '/user-follow',
-            name: 'UserFollow',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/follows/index.vue')
-        },
-        {
-            path: '/user-followeds',
-            name: 'UserFolloweds',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/followeds/index.vue')
-        },
-        {
-            path: '/user-detail',
-            name: 'UserDetail',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/user/index.vue')
-        },
-        {
-            path: '/play-list-detail',
-            name: 'PlayListDetail',
-            meta: { isLogin: true },
-            component: () => import('@/view/user-main/play-list/play-list-detail/index.vue')
-        },
-        {
-            path: '/radio-detail',
-            name: 'RadioDetail',
-            meta: { isLogin: false },
-            component: () => import('@/view/music-hall/audio-station/radio-detail/index.vue')
-        },
-        {
-            path: '/ranking-detail',
-            name: 'RankingDetail',
-            meta: { isLogin: false },
-            component: () => import('@/view/music-hall/ranking/ranking-detail/index.vue')
-        },
-        ]
-    }
-]
+  {
+    path: "/",
+    name: "Index",
+    meta: {},
+    component: () => import("@/layout/index.vue"),
+    children: [
+      {
+        path: "/",
+        name: "Recommend",
+        meta: { isLogin: true },
+        component: () => import("@/view/recommend/index.vue"),
+      },
+      {
+        path: "/music-hall",
+        name: "MusicHall",
+        meta: { isLogin: false },
+        component: () => import("@/view/music-hall/index.vue"),
+      },
+      {
+        path: "/video",
+        name: "Video",
+        meta: { isLogin: false },
+        component: () => import("@/view/video/index.vue"),
+      },
+      {
+        path: "/radio-station",
+        name: "RadioStation",
+        meta: { isLogin: false },
+        component: () => import("@/view/radio-station/index.vue"),
+      },
+      {
+        path: "/i-like",
+        name: "ILike",
+        meta: { isLogin: true },
+        component: () => import("@/view/i-like/index.vue"),
+      },
+      {
+        path: "/local-download",
+        name: "LocalDownload",
+        meta: { isLogin: false },
+        component: () => import("@/view/local-download/index.vue"),
+      },
+      {
+        path: "/recently-played",
+        name: "RecentlyPlayed",
+        meta: { isLogin: false },
+        component: () => import("@/view/recently-played/index.vue"),
+      },
+      {
+        path: "/audition-list",
+        name: "AuditionList",
+        meta: { isLogin: false },
+        component: () => import("@/view/audition-list/index.vue"),
+      },
+      {
+        path: "/purchased-music",
+        name: "PurchasedMusic",
+        meta: { isLogin: true },
+        component: () => import("@/view/purchased-music/index.vue"),
+      },
+      {
+        path: "/play-mv",
+        name: "PlayMv",
+        meta: { isLogin: false },
+        component: () => import("@/view/mv/index.vue"),
+      },
+      {
+        path: "/music-comment",
+        name: "MusicComment",
+        meta: { isLogin: false },
+        component: () => import("@/view/music-comment/index.vue"),
+      },
+      {
+        path: "/theme",
+        name: "Theme",
+        meta: { isLogin: false },
+        component: () => import("@/view/theme/index.vue"),
+      },
+      {
+        path: "/user-main",
+        name: "UserMain",
+        meta: { isLogin: true },
+        component: () => import("@/view/user-main/index.vue"),
+      },
+      {
+        path: "/songer-detail",
+        name: "SongerDetail",
+        meta: { isLogin: false },
+        component: () => import("@/components/songer/index.vue"),
+      },
+      {
+        path: "/songer-album-detail",
+        name: "AlbumDetail",
+        meta: { isLogin: false },
+        component: () =>
+          import("@/components/songer/album/album-detail/index.vue"),
+      },
+      {
+        path: "/user-level-detail",
+        name: "UserLevelDetail",
+        meta: { isLogin: true },
+        component: () => import("@/view/user-main/level/index.vue"),
+      },
+      {
+        path: "/user-follow",
+        name: "UserFollow",
+        meta: { isLogin: true },
+        component: () => import("@/view/user-main/follows/index.vue"),
+      },
+      {
+        path: "/user-followeds",
+        name: "UserFolloweds",
+        meta: { isLogin: true },
+        component: () => import("@/view/user-main/followeds/index.vue"),
+      },
+      {
+        path: "/user-detail",
+        name: "UserDetail",
+        meta: { isLogin: true },
+        component: () => import("@/view/user-main/user/index.vue"),
+      },
+      {
+        path: "/play-list-detail",
+        name: "PlayListDetail",
+        meta: { isLogin: true },
+        component: () =>
+          import("@/view/user-main/play-list/play-list-detail/index.vue"),
+      },
+      {
+        path: "/radio-detail",
+        name: "RadioDetail",
+        meta: { isLogin: false },
+        component: () =>
+          import("@/view/music-hall/audio-station/radio-detail/index.vue"),
+      },
+      {
+        path: "/ranking-detail",
+        name: "RankingDetail",
+        meta: { isLogin: false },
+        component: () =>
+          import("@/view/music-hall/ranking/ranking-detail/index.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: publicRoutes
-})
+  history: createWebHashHistory(),
+  routes: publicRoutes,
+});
 
-export default router
+export default router;
